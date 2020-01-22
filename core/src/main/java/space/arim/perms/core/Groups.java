2d2bcf0e570c53c26702f8c230d726cc85b4f7dc
A248 <anandebeh@gmail.com>
2020-01-21 20:39:48 -0500

    Add UserManager and GroupManager implementations

---
+++ core/src/main/java/space/arim/perms/core/Groups.java
@@ -0,0 +1,69 @@
+/* 
+ * ArimPerms-core
+ * Copyright © 2020 Anand Beh <https://www.arim.space>
+ * 
+ * ArimPerms-core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * ArimPerms-core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with ArimPerms-core. If not, see <https://www.gnu.org/licenses/>
+ * and navigate to version 3 of the GNU General Public License.
+ */
+package space.arim.perms.core;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.eclipse.jdt.annotation.Nullable;
+
+import space.arim.perms.api.ArimPerms;
+import space.arim.perms.api.Group;
+import space.arim.perms.api.GroupManager;
+
+public class Groups implements GroupManager {
+
+	private final ArimPerms core;
+	
+	private final ConcurrentHashMap<String, Group> groups = new ConcurrentHashMap<String, Group>();
+	
+	public Groups(ArimPerms core) {
+		this.core = core;
+	}
+	
+	@Override
+	public Group getGroup(String id) {
+		groups.putIfAbsent(id, new GroupInfo(id));
+		return groups.get(id);
+	}
+	
+	@Override
+	@Nullable
+	public Group getPossibleGroup(String id) {
+		return groups.get(id);
+	}
+	
+	@Override
+	public boolean addGroup(Group group) {
+		groups.put(group.getId(), group);
+		return true;
+	}
+	
+	@Override
+	public void reload(boolean first) {
+		if (first) {
+			core.data().loadGroups().forEach(this::addGroup);
+		}
+	}
+	
+	@Override
+	public void close() {
+		core.data().saveGroups(groups.values());
+	}
+	
+}
---
+++ core/src/main/java/space/arim/perms/core/Users.java
@@ -0,0 +1,66 @@
+/* 
+ * ArimPerms-core
+ * Copyright © 2020 Anand Beh <https://www.arim.space>
+ * 
+ * ArimPerms-core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * ArimPerms-core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with ArimPerms-core. If not, see <https://www.gnu.org/licenses/>
+ * and navigate to version 3 of the GNU General Public License.
+ */
+package space.arim.perms.core;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import space.arim.perms.api.ArimPerms;
+import space.arim.perms.api.User;
+import space.arim.perms.api.UserManager;
+
+public class Users implements UserManager {
+
+	private final ArimPerms core;
+	
+	private final ConcurrentHashMap<String, User> users = new ConcurrentHashMap<String, User>();
+	
+	public Users(ArimPerms core) {
+		this.core = core;
+	}
+	
+	@Override
+	public User getUser(String id) {
+		users.putIfAbsent(id, new UserInfo(id));
+		return users.get(id);
+	}
+	
+	@Override
+	public User getPossibleUser(String id) {
+		return users.get(id);
+	}
+	
+	@Override
+	public boolean addUser(User user) {
+		users.put(user.getId(), user);
+		return true;
+	}
+	
+	@Override
+	public void reload(boolean first) {
+		if (first) {
+			core.data().loadUsers().forEach(this::addUser);
+		}
+	}
+	
+	@Override
+	public void close() {
+		core.data().saveUsers(users.values());
+	}
+	
+}
